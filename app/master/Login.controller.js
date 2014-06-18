349f081a38aa1d5fad89edd345f514bff1c8a24a
frank3b <elverb@gmail.com>
2014-06-18 11:13:59 -0500

    asdsd

--- app/master/Login.controller.js
+++ app/master/Login.controller.js
@@ -2,16 +2,6 @@
 sap.ui.controller("app.master.Login", {
 
 	onInit : function() {
-		//FIXME - remove when no use Kinvey
-		var promise = Kinvey.init({
-		    appKey    : 'kid_VTS8kUwpI9',
-		    appSecret : 'a34280b4eff141aa94bdaf9aef12a48a'
-		});
-
-		promise.then( function() {
-			getPetitioners();
-			getProducts();	
-		});
 		
 	},
 	
