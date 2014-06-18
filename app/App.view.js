82554d38bd3e147999cdab1face0d56de52f278a
frank3b <elverb@gmail.com>
2014-06-18 11:01:57 -0500

    asd

--- app/App.view.js
+++ app/App.view.js
@@ -78,16 +78,6 @@ sap.ui.jsview("app.App", {
         this.app.toMaster("Menu");
         */
 		
-		//FIXME - remove when no use Kinvey
-		var promise = Kinvey.init({
-		    appKey    : 'kid_VTS8kUwpI9',
-		    appSecret : 'a34280b4eff141aa94bdaf9aef12a48a'
-		});
-
-		promise.then( function() {
-			getPetitioners();
-			getProducts();	
-		});
 
         return this.splitApp;
     }
